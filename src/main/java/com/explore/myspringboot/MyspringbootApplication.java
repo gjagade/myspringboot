3d6d0d3bda292008f938858dea45e68b9bbb7814
G Jagadeesh <gjagade@yahoo.com>
2019-03-03 22:08:43 -0500

    minor changes`

--- src/main/java/com/explore/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/explore/myspringboot/MyspringbootApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MyspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringbootApplication.class, args);
+		System.out.println("This is a test");
 	}
 
 }
